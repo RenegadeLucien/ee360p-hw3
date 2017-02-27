af7def3578992868dea7be045e94bd4732a34c7b
rajatkalyan1 <rajatkalyan1@users.noreply.github.com>
2017-02-26 18:39:32 -0600

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,5 +1,6 @@
-package hw3;
-
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.io.*;
 import java.util.Scanner;
 
 public class Client {
@@ -30,7 +31,6 @@ public class Client {
 	      if (tokens[0].equals("setmode")) {
 	        // TODO: set the mode of communication for sending commands to the server 
 	        // and display the name of the protocol that will be used in future
-	    	  
 	    	  if(tokens[1].equals("U")){
 	    		  isTCP = false;
 	    		  System.out.println("Mode: UDP");
@@ -46,38 +46,162 @@ public class Client {
 	      else if (tokens[0].equals("purchase")) {
 	        // TODO: send appropriate command to the server and display the
 	        // appropriate responses form the server
+	    	  String userName = tokens[1];
+    		  String item = tokens[2];
+    		  String quantity = tokens[3];
+    		  String toSend = "purchase " + userName + " " + item + " " + quantity;
 	    	  if(isTCP){
-	    		  
+	    		  String messageReceived;
+	    		  try{
+	    			  Socket socket = new Socket(hostAddress, tcpPort);
+	    			  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+	    			  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    			  out.writeBytes(toSend + '\n');
+	    			  messageReceived = in.readLine();
+	    			  System.out.println("Received from Server: " + messageReceived);
+	    			  socket.close();
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}	    		  
 	    	  }
 	    	  else{
-	    		  
+	    		  DatagramPacket rPacket, sPacket;
+	    		  int len = 1024;
+	    		  byte[] rBuffer = new byte[len];
+	    		  try{
+	    			  InetAddress ia = InetAddress.getByName(hostAddress);
+	    			  DatagramSocket datasocket = new DatagramSocket();
+	    			  byte[] buffer = new byte[toSend.length()];
+	    			  buffer = toSend.getBytes();
+	    			  sPacket = new DatagramPacket(buffer, buffer.length, ia, udpPort);
+	    			  datasocket.send(sPacket);
+	    			  rPacket = new DatagramPacket(rBuffer, rBuffer.length);
+	    			  datasocket.receive(rPacket);
+	    			  String retstring = new String(rPacket.getData(), 0, rPacket.getLength());
+	    			  System.out.println("Received from Server: " + retstring); 
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}
 	    	  }
 	      } else if (tokens[0].equals("cancel")) {
 	        // TODO: send appropriate command to the server and display the
 	        // appropriate responses form the server
+	    	  String ID = tokens[1];
+	    	  String toSend = "cancel " + ID;
 	    	  if(isTCP){
-	    		  
+	    		  String messageReceived;
+	    		  try{
+	    			  Socket socket = new Socket(hostAddress, tcpPort);
+	    			  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+	    			  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    			  out.writeBytes(toSend + '\n');
+	    			  messageReceived = in.readLine();
+	    			  System.out.println("Received from Server: " + messageReceived);
+	    			  socket.close();
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}	 
 	    	  }
 	    	  else{
-	    		  
+	    		  DatagramPacket rPacket, sPacket;
+	    		  int len = 1024;
+	    		  byte[] rBuffer = new byte[len];
+	    		  try{
+	    			  InetAddress ia = InetAddress.getByName(hostAddress);
+	    			  DatagramSocket datasocket = new DatagramSocket();
+	    			  byte[] buffer = new byte[toSend.length()];
+	    			  buffer = toSend.getBytes();
+	    			  sPacket = new DatagramPacket(buffer, buffer.length, ia, udpPort);
+	    			  datasocket.send(sPacket);
+	    			  rPacket = new DatagramPacket(rBuffer, rBuffer.length);
+	    			  datasocket.receive(rPacket);
+	    			  String retstring = new String(rPacket.getData(), 0, rPacket.getLength());
+	    			  System.out.println("Received from Server: " + retstring); 
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}
 	    	  }
 	      } else if (tokens[0].equals("search")) {
 	        // TODO: send appropriate command to the server and display the
 	        // appropriate responses form the server
+	    	  String user = tokens[1];
+	    	  String toSend = "search " + user;
 	    	  if(isTCP){
-	    		  
+	    		  String messageReceived;
+	    		  try{
+	    			  Socket socket = new Socket(hostAddress, tcpPort);
+	    			  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+	    			  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    			  out.writeBytes(toSend + '\n');
+	    			  messageReceived = in.readLine();
+	    			  System.out.println("Received from Server: " + messageReceived);
+	    			  socket.close();
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}	 
 	    	  }
 	    	  else{
-	    		  
+	    		  DatagramPacket rPacket, sPacket;
+	    		  int len = 1024;
+	    		  byte[] rBuffer = new byte[len];
+	    		  try{
+	    			  InetAddress ia = InetAddress.getByName(hostAddress);
+	    			  DatagramSocket datasocket = new DatagramSocket();
+	    			  byte[] buffer = new byte[toSend.length()];
+	    			  buffer = toSend.getBytes();
+	    			  sPacket = new DatagramPacket(buffer, buffer.length, ia, udpPort);
+	    			  datasocket.send(sPacket);
+	    			  rPacket = new DatagramPacket(rBuffer, rBuffer.length);
+	    			  datasocket.receive(rPacket);
+	    			  String retstring = new String(rPacket.getData(), 0, rPacket.getLength());
+	    			  System.out.println("Received from Server: " + retstring); 
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}
 	    	  }
 	      } else if (tokens[0].equals("list")) {
 	        // TODO: send appropriate command to the server and display the
 	        // appropriate responses form the server
 	    	  if(isTCP){
-	    		  
+	    		  String messageReceived;
+	    		  try{
+	    			  Socket socket = new Socket(hostAddress, tcpPort);
+	    			  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+	    			  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    			  out.writeBytes(tokens[0] + '\n');
+	    			  messageReceived = in.readLine();
+	    			  System.out.println("Received from Server: " + messageReceived);
+	    			  socket.close();
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}	 
 	    	  }
 	    	  else{
-	    		  
+	    		  DatagramPacket rPacket, sPacket;
+	    		  int len = 1024;
+	    		  byte[] rBuffer = new byte[len];
+	    		  try{
+	    			  InetAddress ia = InetAddress.getByName(hostAddress);
+	    			  DatagramSocket datasocket = new DatagramSocket();
+	    			  byte[] buffer = new byte[tokens[0].length()];
+	    			  buffer = tokens[0].getBytes();
+	    			  sPacket = new DatagramPacket(buffer, buffer.length, ia, udpPort);
+	    			  datasocket.send(sPacket);
+	    			  rPacket = new DatagramPacket(rBuffer, rBuffer.length);
+	    			  datasocket.receive(rPacket);
+	    			  String retstring = new String(rPacket.getData(), 0, rPacket.getLength());
+	    			  System.out.println("Received from Server: " + retstring); 
+	    		  }
+	    		  catch (UnknownHostException e) {System.err.println(e);} 
+	    		  catch (SocketException e) {System.err.println(e);}
+	    		  catch (IOException e){System.err.println(e);}
 	    	  }
 	      } else {
 	        System.out.println("ERROR: No such command");
