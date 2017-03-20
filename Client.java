3dcf74dfa59f16d7b8c903b71675f3b7ea1b93fd
Michael M <michael.marino.1337@gmail.com>
2017-03-20 18:52:06 -0500

    client done (untested)

--- Client.java
+++ Client.java
@@ -7,10 +7,15 @@ public class Client {
 	public static void main (String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    int numServer = sc.nextInt();
-	    
+	    String[] hostAddresses = new String[numServer];
+	    int[] tcpPorts = new int[numServer];
+	    //TODO: parse the ips and ports of servers
 	    for (int i = 0; i < numServer; i++) {
-	      // TODO: parse inputs to get the ips and ports of servers
+	      String[] line = sc.nextLine().split(":");
+	      hostAddresses[i] = line[0];
+	      tcpPorts[i] = Integer.parseInt(line[1]);
 	    }
+	    int i = 0;
 	    while(sc.hasNextLine()) {
 	      String cmd = sc.nextLine();
 	      String[] tokens = cmd.split(" ");
@@ -19,9 +24,11 @@ public class Client {
     		  String item = tokens[2];
     		  String quantity = tokens[3];
     		  String toSend = "purchase " + userName + " " + item + " " + quantity;
-    		  String messageReceived;
+    		  String messageReceived = null;
+    		  while (messageReceived == null && i < numServer)
+    		  {
 	    	  try{
-	    		  Socket socket = new Socket(hostAddress, tcpPort);
+	    		  Socket socket = new Socket(hostAddresses[i], tcpPorts[i]);
 	    		  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 	    		  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	    		  out.writeBytes(toSend + '\n');
@@ -31,13 +38,17 @@ public class Client {
 	    	  }
 	    	  catch (UnknownHostException e) {System.err.println(e);} 
 	    	  catch (SocketException e) {System.err.println(e);}
-	    	  catch (IOException e){System.err.println(e);}	    		  
+	    	  catch (IOException e){System.err.println(e);}
+	    	  i++;
+    		  }
 	      } else if (tokens[0].equals("cancel")) {
 	    	  String ID = tokens[1];
 	    	  String toSend = "cancel " + ID;
-	    	  String messageReceived;
+	    	  String messageReceived = null;
+	    	  while (messageReceived == null && i < numServer)
+	    	  {
 	    	  try{
-	    		  Socket socket = new Socket(hostAddress, tcpPort);
+	    		  Socket socket = new Socket(hostAddresses[i], tcpPorts[i]);
 	    		  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 	    		  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	    		  out.writeBytes(toSend + '\n');
@@ -47,13 +58,17 @@ public class Client {
 	    	  }
 	    	  catch (UnknownHostException e) {System.err.println(e);} 
 	    	  catch (SocketException e) {System.err.println(e);}
-	    	  catch (IOException e){System.err.println(e);}	 
+	    	  catch (IOException e){System.err.println(e);}
+	    	  i++;
+    		  }
 	      } else if (tokens[0].equals("search")) {
 	    	  String user = tokens[1];
 	    	  String toSend = "search " + user;
-	    	  String messageReceived;
+	    	  String messageReceived = null;
+	    	  while (messageReceived == null && i < numServer)
+	    	  {
 	    	  try{
-	    		  Socket socket = new Socket(hostAddress, tcpPort);
+	    		  Socket socket = new Socket(hostAddresses[i], tcpPorts[i]);
 	    		  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 	    		  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	    		  out.writeBytes(toSend + '\n');
@@ -64,10 +79,15 @@ public class Client {
 	    	  }
 	    	  catch (UnknownHostException e) {System.err.println(e);} 
 	    	  catch (SocketException e) {System.err.println(e);}
-	    	  catch (IOException e){System.err.println(e);}	 
+	    	  catch (IOException e){System.err.println(e);}
+	    	  i++;
+    		  }
 	      } else if (tokens[0].equals("list")) {
+	    	  boolean messageReceived = false;
+	    	  while (messageReceived == false && i < numServer)
+	    	  {
 	    	  try{
-	    		  Socket socket = new Socket(hostAddress, tcpPort);
+	    		  Socket socket = new Socket(hostAddresses[i], tcpPorts[i]);
 	    		  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 	    		  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	    		  out.writeBytes(tokens[0] + '\n');
@@ -75,10 +95,13 @@ public class Client {
 	    		    while ((line = in.readLine()) != null)
 	    		    	System.out.println(line);
 	    		  socket.close();
+	    		  messageReceived = true;
 	    	  }
 	    	  catch (UnknownHostException e) {System.err.println(e);} 
 	    	  catch (SocketException e) {System.err.println(e);}
 	    	  catch (IOException e){System.err.println(e);}	 
+	    	  i++;
+    		  }
 	      } else {
 	        System.out.println("ERROR: No such command");
 	      }
